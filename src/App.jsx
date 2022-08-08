aff2bb2c55d1f9df72c516d665aa5b1b51433b9f
Prakhar Sankrityayan <prakhar3099@gmail.com>
2022-08-08 12:53:15 +0530

    Fully functioning with redirect to books page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './screens/Login';
 
 const App = () => {
-  return <Login />
+  return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  </Router>
+  );
 };
 
 export default App;
