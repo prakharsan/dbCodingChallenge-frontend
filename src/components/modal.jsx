36385e4f23a80a90315994b17ae5a2249f810753
Prakhar Sankrityayan <prakhar3099@gmail.com>
2022-08-08 17:41:33 +0530

    feat: modal with props and materail ui buttons

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
+import { Stack, Button } from "@mui/material";
 
 Modal.setAppElement("#root");
-function InfoModal() {
+function InfoModal(props) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
     <div>
@@ -16,17 +17,28 @@ function InfoModal() {
           },
           content: {
             color: 'black',
-            height: '50%',
+            height: '70%',
             width: '50%',
             position: 'absolute',
             left:'25%',
-            top:'25%'
+            top:'15%'
           },
         }}
       >
-        <h2>Modal title</h2>
-        <p>Modal Body</p>
-        <button onClick={() => setModalIsOpen(false)}>Close</button>
+        <h1>{props.name}</h1>
+        <hr/>
+        <h3>ISIN: {props.isin}</h3>
+        <h3>CUSIP: {props.cusip}</h3>
+        <h3>Issuer: {props.issuer}</h3>
+        <h3>Maturity Date: {props.maturityDate.toString()}</h3>
+        <h3>Coupon: {props.coupon}</h3>
+        <h3>Type: {props.type}</h3>
+        <h3>Face Value: {props.faceValue}</h3>
+        <h3>Status: {props.status}</h3>
+        <Stack spacing={2} direction='row'>
+        <Button variant='contained' onClick={() => setModalIsOpen(false)}>Close</Button>
+        <Button variant='contained' onClick={() => setModalIsOpen(false)}>Review</Button>
+        </Stack>
       </Modal>
     </div>
   );
