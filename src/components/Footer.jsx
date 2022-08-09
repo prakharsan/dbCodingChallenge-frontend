e6157e68f380ee21dd1e91906720cf646037a799
Muskan <muskanjaiswal710@gmail.com>
2022-08-09 13:14:52 +0530

    modal edited and header footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,35 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { BottomNavigation } from '@mui/material';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Icon from "@mui/material/Icon";
+import { BottomNavigation } from "@mui/material";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
 
- const Footer = () => {
+const Footer = () => {
   return (
-    <div> 
-      <Box sx={{ flexGrow: 2 }} >
-    <AppBar   position="static"style={{bottom:0, position: 'fixed'}}>
-      <Toolbar>
-        
-        <Typography variant="p" component="div" sx={{ flexGrow: 1 }}>
-          Contact
-        </Typography>
-        <Typography variant="p" component="div" sx={{ flexGrow: 1 }}>
-          HelpDesk
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </Box>
-  <BottomNavigation/></div>
+    <div>
+      <Box sx={{ flexGrow: 2 }}>
+        <AppBar position="static" style={{ bottom: 0, position: "fixed" }}>
+          <Toolbar>
+            <Typography variant="p" component="div" sx={{ flexGrow: 1 }}>
+              Copyright © 2022 FIC Portal
+            </Typography>
+            <Icon size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+              <ContactMailIcon />
+            </Icon>
+            <Typography variant="p" component="div" sx={{ flexGrow: 1 }}>
+              Contact:{" "}
+              <a href="mailto:ficadmin@fic.com" style={{ color: "white" }}>
+                ficadmin@fic.com
+              </a>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <BottomNavigation />
+    </div>
   );
-}
+};
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,42 +4,51 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import Icon from "@mui/material/Icon";
+import MenuBook from "@mui/icons-material/MenuBook";
 import { useLocation, useNavigate, Link } from "react-router-dom";
-
 
 export default function Header(props) {
   const location = useLocation();
   const navigate = useNavigate();
-  function logoutHandler(){
-    localStorage.removeItem('username');
-    localStorage.removeItem('password');
+  function logoutHandler() {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
     navigate("/");
   }
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
+          <Icon
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
-          </IconButton>
+            <MenuBook />
+          </Icon>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {location.pathname.split("/")[1] === "trades" ? (
-              <Link to="/books" style={{ textDecoration: 'none', color: 'white'}}>Back</Link>
+              <Link
+                to="/books"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Back
+              </Link>
             ) : (
               "FIC Team"
             )}
           </Typography>
-          {localStorage.getItem('username') && <Button color="inherit">{localStorage.getItem('username')}</Button>}
-          {localStorage.getItem('username') && <Button color="inherit" onClick={() => logoutHandler()}>Logout</Button>}
-
+          {localStorage.getItem("username") && (
+            <Button color="inherit">{localStorage.getItem("username")}</Button>
+          )}
+          {localStorage.getItem("username") && (
+            <Button color="inherit" onClick={() => logoutHandler()}>
+              Logout
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
